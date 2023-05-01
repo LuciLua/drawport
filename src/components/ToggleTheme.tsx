861f28e961b5203a4619b503138d40d4e458a9a5
Luci Lua <luci.lua81@gmail.com>
2023-05-01 15:00:31 -0300

    [fix] 🌑 dark mode localStorage

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -1,36 +1,29 @@
 'use client'
 
 import { BsMoonStars } from "react-icons/bs"
-import { useState, useEffect } from "react"
+import { useEffect } from "react"
 
 function ToggleTheme() {
 
-    const [theme, setTheme] = useState<'light' | 'dark' | any>(localStorage.getItem('theme') || 'dark');
-
-    function light() {
-        localStorage.setItem('theme', 'light');
-        document.body.classList.remove('dark');
-        document.body.classList.add('light');
+    function changeDOM(theme: 'dark' | 'light' | string) {
+        const removeAndAddClasses = ([c_remove, c_add]) => { document.body.classList.remove(c_remove); document.body.classList.add(c_add) }
+        theme === 'dark' ? removeAndAddClasses(['light', 'dark']) : removeAndAddClasses(['dark', 'light'])
     }
 
-    function dark() {
-        localStorage.setItem('theme', 'dark');
-        document.body.classList.remove('light');
-        document.body.classList.add('dark');
+    function handleTheme() {
+        const local_theme = localStorage.getItem('theme')
+        const localSetItemAndChangeDOM = ([item, mode]) => { localStorage.setItem(item, mode); changeDOM(mode) }
+        local_theme === 'dark' ? localSetItemAndChangeDOM(['theme', 'light']) : localSetItemAndChangeDOM(['theme', 'dark'])
     }
 
     useEffect(() => {
-        theme === 'dark' ? dark() : light()
-    }, [theme]);
-
-    function toggleClass() {
-        const newTheme = theme === 'dark' ? 'light' : 'dark';
-        setTheme(newTheme);
-    }
+        const local_theme = localStorage.getItem('theme')
+        changeDOM(local_theme)
+    }, [])
 
     return (
         <div className="absolute w-[30px] h-[30px] shadow-lg rounded-lg bg-slate-50 text-slate-500 font-normal flex justify-center items-center cursor-pointer hover:bg-[#5a5b65] hover:text-[#fff]"
-            onClick={toggleClass}>
+            onClick={handleTheme}>
             <BsMoonStars />
         </div>
     )
