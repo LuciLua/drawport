b4c9c3bad99b1c2e9820619ab185eb354a535161
Luci Lua <luci.lua81@gmail.com>
2023-06-10 20:27:26 -0300

    replace path of fetchData projects

--- src/app/curtas/page.tsx
+++ src/app/curtas/page.tsx
@@ -8,7 +8,7 @@ type Project = { id: number; name: string; description: string; link?: string; i
 async function curtas() {
 
   async function fetchProjects() {
-    const fetchData:any = await fetch('/data/projects.json',
+    const fetchData:any = await fetch('http://localhost:3000/data/projects.json' || `http://${window.location.href}/data/projects.json`,
       {
         next: {
           revalidate: 10
