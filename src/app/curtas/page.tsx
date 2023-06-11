fd1318978fbc7d2981285970cbee01d642020ab5
Luci Lua <luci.lua81@gmail.com>
2023-06-10 21:15:06 -0300

    add only data

--- src/app/curtas/page.tsx
+++ src/app/curtas/page.tsx
@@ -8,31 +8,31 @@ type Project = { id: number; name: string; description: string; link?: string; i
 
 async function curtas() {
 
-  async function fetchProjects() {
-    // const fetchData = await axios.get('/data/projects.json')
-    const fetchData:any = await fetch(`https://lucilua-film-portfolio.vercel.app/data/projects.json`,
-      {
-        next: {
-          revalidate: 10
-        }
-      })
-    const data = await fetchData.json()
-    // const data = await fetchData.data()
-    const proj = await data.projetos
-    return proj
-  }
+  // async function fetchProjects() {
+  //   // const fetchData = await axios.get('/data/projects.json')
+  //   const fetchData:any = await fetch(`https://lucilua-film-portfolio.vercel.app/data/projects.json`,
+  //     {
+  //       next: {
+  //         revalidate: 10
+  //       }
+  //     })
+  //   const data = await fetchData.json()
+  //   // const data = await fetchData.data()
+  //   const proj = await data.projetos
+  //   return proj
+  // }
 
-  useEffect(() => {
-    fetchProjects()
-  }, [])
+  // useEffect(() => {
+  //   fetchProjects()
+  // }, [])
 
-  const projects: Project[] = await fetchProjects()
+  // const projects: Project[] = await fetchProjects()
 
 
   return (
     <div className="p-10">
       <div className="bg-slate-300 flex gap-10">
-        {
+        {/* {
           projects ? 
           projects.map((p) => {
             return (
@@ -49,7 +49,7 @@ async function curtas() {
           })
           : 
           null
-        }
+        } */}
       </div>
     </div>
   );
