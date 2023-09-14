90f0504cd459f5638a09b3d813df732a65b4a3a2
Luci Lua <luci.lua81@gmail.com>
2023-09-14 16:46:18 -0300

    fix 2

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -5,12 +5,12 @@ import { TProjetos } from "../../types/TProjetos";
 
 async function projetos() {
 
-  // var path = 'http://lucilua-film-portfolio.vercel.app/api/projects'
+  var path = 'http://lucilua-film-portfolio.vercel.app/api/projects'
   // const path = `http://localhost:3000/api/projects`
 
-  // const response = await fetch(path, { next: { revalidate: 180 } })
-  // const jsonResponse = await response.json()
-  // const projects: TProjetos[] = await jsonResponse.projetos
+  const response = await fetch(path, { next: { revalidate: 180 } })
+  const jsonResponse = await response.json()
+  const projects: TProjetos[] = await jsonResponse.projetos
 
   return (
     <div className="p-10 flex justify-center items-center flex-col max-w-max-w-content m-auto">
@@ -18,7 +18,7 @@ async function projetos() {
       <div className="flex flex-wrap gap-1 justify-center">
         <Suspense fallback={Array.from({ length: 4 }).map((v, i) => <SkeletonCardProject key={i} />)}>
           {/* @ts-ignore comment above the line of code that is causing the error */}
-          {/* {projects && projects.map((p: TProjetos) => <ProjetosCards projeto={p} key={p._id} />)} */}
+          {projects && projects.map((p: TProjetos) => <ProjetosCards projeto={p} key={p._id} />)}
         </Suspense>
       </div>
     </div>
